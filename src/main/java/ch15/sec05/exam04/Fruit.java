03a47df534cc5c3ba93805db12f88585f93352a0
seohyunk09 <2022112400@dgu.ac.kr>
2025-04-30 17:30:19 +0900

    ch15 추가

---
+++ src/main/java/ch15/sec05/exam04/Fruit.java
@@ -0,0 +1,10 @@
+package ch15.sec05.exam04;
+
+public class Fruit {
+    public String name;
+    public int price;
+    public Fruit(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
